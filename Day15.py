66aec4278bd40f74797c0feabe1aaeac133a5990
Cory Burmahl <beenters@gmail.com>
2024-07-09 09:07:53 -0700

    Day 15

--- Day15.py
+++
@@ -1,4 +0,0 @@
-"""
-Day 15 - Factorial
-Write a function to calculate the factorial of a number.
-"""
---
+++ completed/Day15.py
@@ -0,0 +1,9 @@
+"""
+Day 15 - Factorial
+Write a function to calculate the factorial of a number.
+"""
+
+import math
+
+fact = int(input("Enter Number: "))
+print(math.factorial(fact))
