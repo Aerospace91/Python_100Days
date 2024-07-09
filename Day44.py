8449e05697190cfd3f660973af0dfe357844644c
Cory Burmahl <beenters@gmail.com>
2024-07-09 12:21:16 -0700

    Day 44

--- Day44.py
+++
@@ -1,4 +0,0 @@
-"""
-Day 44 - Class definition
-Create a class for a book with attributes like title and author.
-"""
---
+++ completed/Day44.py
@@ -0,0 +1,9 @@
+"""
+Day 44 - Class definition
+Create a class for a book with attributes like title and author.
+"""
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
